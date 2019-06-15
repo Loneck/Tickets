b891c3b31949bac815df482c8e3f164277461e2d
ltorrejon <leo.torrejon@gmail.com>
2019-06-14 23:33:56 -0400

    add model

--- ticketsystem/tickets/admin.py
+++ ticketsystem/tickets/admin.py
@@ -2,5 +2,21 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Ticket
+
 
 # Register your models here.
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    filter = (
+        'title',
+        'description',
+        'status'
+    )
+    list_display = (
+        'id',
+        'title',
+        'description',
+        'status',
+        'created_at'
+    )
--- ticketsystem/tickets/models.py
+++ ticketsystem/tickets/models.py
@@ -4,3 +4,23 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+TICKET_STATUS = (
+    ('OPEN', 'Abierto'),
+    ('PENDING', 'Pendiete'),
+    ('IN_PROCESS', 'En Proceso'),
+    ('CLOSED', 'Resuelto y cerrado')
+)
+
+
+class Ticket(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    status = models.CharField(max_length=10, choices=TICKET_STATUS)
+    created_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Ticket"
+        verbose_name_plural = "Tickets"
+
+    def __unicode__(self):
+        return self.title
